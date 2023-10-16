f5f3b0a7f33b78f202458c20b71dabe8fbf5c011
otanerifpb <renato.ribeiro.pro@gmail.com>
2023-10-16 16:45:57 -0300

    Atualização Add Lista Médico

--- hospital/src/main/java/br/edu/ifpb/pdist/hospital/controller/MedicoController.java
+++ hospital/src/main/java/br/edu/ifpb/pdist/hospital/controller/MedicoController.java
@@ -2,6 +2,7 @@ package br.edu.ifpb.pdist.hospital.controller;
 
 import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,11 +12,16 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/medicos")
 public class MedicoController {
 
+    // Ativa o menu Médico na barra de navegação
+    @ModelAttribute("menu")
+    public String activeMenu(){
+        return "medicos";
+    }
+
     // Rota para acessar a lista pelo menu
     @RequestMapping(method = RequestMethod.GET)
     public ModelAndView listAll(ModelAndView mav) {
         mav.setViewName("medicos/listMedico");
         return mav;
-    }
-    
+    } 
 }
---
+++ hospital/src/main/java/br/edu/ifpb/pdist/hospital/model/Medico.java
@@ -0,0 +1,39 @@
+package br.edu.ifpb.pdist.hospital.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+//@EqualsAndHashCode(exclude = {"instituicao", "declaracoes"})
+@AllArgsConstructor
+@Entity
+public class Medico implements Serializable {
+
+    // Para garantir que a assinatura de um número seja única , para o uso do @Id
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    
+    private String nome;
+
+    private String CRM;
+
+    private String sexo;
+
+    private Date dataNascimento;
+
+    private String especialidade;
+
+    private String telefone;
+}
