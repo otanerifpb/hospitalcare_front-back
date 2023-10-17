c5f62266f6a88730590aaab319d4dec72d2d721f
otanerifpb <renato.ribeiro.pro@gmail.com>
2023-10-17 08:35:36 -0300

    Atualização Final CRUD Médico

--- hospital/src/main/java/br/edu/ifpb/pdist/hospital/controller/MedicoController.java
+++ hospital/src/main/java/br/edu/ifpb/pdist/hospital/controller/MedicoController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -71,4 +72,48 @@ public class MedicoController {
         }
         return mav;
     }
+
+    // Rota para acessar o formunlário de atualização ou a lista se não for atualizar 
+    @RequestMapping("/{id}")
+    public ModelAndView getEstudanteById(@PathVariable(value = "id") Integer id, ModelAndView mav) {
+        Optional<Medico> opMedico = medicoRepository.findById(id);
+        if (opMedico.isPresent()) {
+            Medico medico = opMedico.get();
+            mav.addObject("medico", medico);
+            mav.setViewName("medicos/formUpMedico");
+        } else {
+            mav.addObject("errorMensagem", "Médico  não encontrado.");
+            mav.setViewName("medicos/listMedico");
+        }
+        return mav;
+    }
+
+    // Rota para atualizar um Médico na lista pelo formUpMedico
+    @RequestMapping(value="/update", method = RequestMethod.POST)
+    public ModelAndView updade(Medico medico, ModelAndView mav, RedirectAttributes redAttrs) {
+        medicoRepository.save(medico);
+        mav.addObject("medicos", medicoRepository.findAll());
+        //redAttrs.addFlashAttribute("succesMensagem", "Instituição atualizada com sucesso!");
+        //mav.setViewName("redirect:/estudantes");
+        mav.addObject("succesMensagem", "Medico "+medico.getNome()+", atualizado com sucesso!");
+        mav.setViewName("/medicos/listMedico"); 
+        return mav;
+    }
+
+    // Rota para deletar um Médico da lista
+    @RequestMapping("{id}/delete")
+    public ModelAndView deleteById(@PathVariable(value = "id") Integer id, ModelAndView mav, RedirectAttributes redAttrs) {
+        Optional<Medico> opMedico = medicoRepository.findById(id);
+        if (opMedico.isPresent()) {
+            Medico medico = opMedico.get();
+            medicoRepository.deleteById(id);
+            redAttrs.addFlashAttribute("succesMensagem", "Medico "+medico.getNome()+" deletado com sucesso!!");
+        } else {
+            redAttrs.addFlashAttribute("errorMensagem", "Medico Não encontrado!!");
+        }
+        mav.setViewName("redirect:/medicos");
+        return mav;
+    }
+
+
 }
--- hospital/src/main/java/br/edu/ifpb/pdist/hospital/model/Medico.java
+++ hospital/src/main/java/br/edu/ifpb/pdist/hospital/model/Medico.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Past;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -34,6 +35,7 @@ public class Medico implements Serializable {
     private String sexo;
 
     @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @Past(message = "Data deve ser no passado")
     private Date dataNascimento;
 
     private String especialidade;
